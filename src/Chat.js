9cca86d08a48838250ecee41853cb9b5073ac0a6
yunhoe <yunhoe@deepnatural.ai>
2020-12-01 01:18:30 +0900

    Chat component is finished

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import './Chat.css'
 import ChatHeader from './ChatHeader'
+import AddCircle from '@material-ui/icons/AddCircle'
+import CardGiftcard from '@material-ui/icons/CardGiftcard'
+import Gif from '@material-ui/icons/Gif'
+import EmojiEmotions from '@material-ui/icons/EmojiEmotions'
+import Message from './Message'
 
 function Chat() {
   return (
     <div className="chat">
       <ChatHeader />
+
+      <div className="chat__messages">
+        <Message />
+      </div>
+      <div className="chat__input">
+        <AddCircle fontSize="large" />
+        <form>
+          <input placeholder={`Message #TESTCHANNEL`} />
+          <button className="chat__inputButton" type="submit">
+            Send Message
+          </button>
+        </form>
+        <div className="chat__inputIcons">
+          <CardGiftcard fontSize="large" />
+          <Gif fontSize="large" />
+          <EmojiEmotions fontSize="large" />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyATEO0BEMD_4nwyDK02UbdQ8fUOAUlO2fw',
+  authDomain: 'discord-clone-efbe5.firebaseapp.com',
+  databaseURL: 'https://discord-clone-efbe5.firebaseio.com',
+  projectId: 'discord-clone-efbe5',
+  storageBucket: 'discord-clone-efbe5.appspot.com',
+  messagingSenderId: '877501078579',
+  appId: '1:877501078579:web:d15915536f5dee4364d746',
+  measurementId: 'G-J2B27NYDQ5',
+}
