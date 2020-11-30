d5fe5be96a065b76288fee237e4266b08f40e0c8
yunhoe <yunhoe@deepnatural.ai>
2020-12-01 00:17:45 +0900

    Chat frame is added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Counter } from './features/Counter'
 import './App.css'
 import Sidebar from './Sidebar'
+import Chat from './Chat'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Sidebar />
+      <Chat />
     </div>
   )
 }
---
+++ src/ChatHeader.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './ChatHeader.css'
+import Notifications from '@material-ui/icons/Notifications'
+import EditLocationRounded from '@material-ui/icons/EditLocationRounded'
+import PeopleAltRounded from '@material-ui/icons/PeopleAltRounded'
+import SearchRounded from '@material-ui/icons/SearchRounded'
+import SendRounded from '@material-ui/icons/SendRounded'
+import HelpRounded from '@material-ui/icons/HelpRounded'
+
+function ChatHeader() {
+  return (
+    <div className="chatHeader">
+      <div className="chatHeader__left">
+        <h3>
+          <span className="chatHeader__hash">#</span>
+          Test Channel Name
+        </h3>
+      </div>
+      <div className="chatHeader__right">
+        <Notifications />
+        <EditLocationRounded />
+        <PeopleAltRounded />
+
+        <div className="chatHeader__search">
+          <input placeholder="Search" />
+          <SearchRounded />
+        </div>
+
+        <SendRounded />
+        <HelpRounded />
+      </div>
+    </div>
+  )
+}
+
+export default ChatHeader
